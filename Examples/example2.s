dc6f01e826b63c3f858cbba3dcb2aa16028a7cb3
Mouadcr7 <46369667+Mouadcr7@users.noreply.github.com>
2021-01-14 11:05:38 +0100

    exemple2.s update

--- Examples/example2.s
+++ Examples/example2.s
@@ -1,16 +1,14 @@
 .global main
 .text
 main:
-    mov r0, #0x12
-    mov r1, #0x34
-    add r0, r1, #0x12
-    mov r1, #0x56
-    add r0, r1, r0, lsl #8
-    mov r1, #0x78
-    add r0, r1, r0, lsl #8
-    mov r1, #0x2000
-    str r0, [r1]
-    ldrb r2, [r1]
-    add r1, r1, #3
-    ldrb r3, [r1]
+    mov r0, #120
+    mov r1, #5
+    add r2, r1, r0, lsl #8
+    add r3, r1, r0
+    add r4 , r1, r2
+    subs r5, r1, r0, lsr #8
+    mov r0,#5
+    tsteq r0, r1
+    cmpne r1, r2
+    mvn r0, #5
     swi 0x123456
