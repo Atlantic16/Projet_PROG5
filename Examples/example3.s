ea02cb6f4ff7e9785ae69160bbf674d619069940
zak-el <62945041+zak-el@users.noreply.github.com>
2021-01-14 11:45:39 +0100

    Add files via upload

--- Examples/example3.s
+++ Examples/example3.s
@@ -2,10 +2,19 @@
 .text
 main:
     ldr r0, =limite
+    mov r4, #1
+    mov r5, #8
+    ldr r4, [r0]
+    str r5, [r0, #4]
+    strh r4,[r0, #8]
+    strb r4,[r0, #12]!
+    str r4,[r0, #4]
     ldrb r1, [r0]
-    add r0, r0, #3
-    ldrb r2, [r0]
+    ldrh r2, [r0]
+    ldr r3, [r0]
+    ldr r6, [r0, r5, LSR #1 ]
     swi 0x123456
 .data
 limite:
     .word 0x12345678
+
